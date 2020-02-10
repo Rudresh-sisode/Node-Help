6e0a7186b7934420a03f0edd9be13a98b4d2ba27
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-10 15:58:11 +0530

    module 3 ashychro

---
+++ express-demo-module3/app.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const app=express();
+const fs=require('fs')
+
+const port=process.env.PORT || 8080;
+
+app.listen(port,()=>{
+    console.log('server is on port %s',port)
+})
+
+//define a route
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/login.html');
+})
+
+app.post('/login',(req,res)=>{
+    res.end('login successfully')
+
+})
+app.post('/loginfail',(req,res)=>{
+    res.end('login fails')
+})
+app.get('/getMovies',(req,res)=>{
+    var data=fs.readFileSync('./movies.json')
+    var movies=JSON.parse(data).movies
+    console.log(movies)
+    res.json(movies[0].name)
+})
