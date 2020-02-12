7d4710ea4a1aafd7eeb41ea35787bac272db255b
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-12 17:17:39 +0530

    ejs very excellent example please prefer

--- express-esj-demo-module3/app.js
+++ express-esj-demo-module3/app.js
@@ -2,11 +2,23 @@ const express=require('express')
 const app=express()
 const port=process.env.PORT || 8080
 app.set('view engine','ejs');
-
+const fs=require('fs')
 app.get('/',(req,res)=>{
-    res.render('login')
+    res.render('login',{title:'FACEBOOK'})
+
+    /*
+    res.end()
+    res.sendFile();
+    res.json();
+    */
 })
 
 app.listen(port,()=>{
     console.log("server is listening on port %s",port);
+})
+
+app.post('/login',(req,res)=>{
+    var stu=fs.readFileSync('./model/students.json')
+    console.log(stu.toString())
+    res.render('homepage',{student:JSON.parse(stu).students})
 })
