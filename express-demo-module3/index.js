d9f54eac7918cb6b5af4a870ef89831885e07497
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-12 12:43:58 +0530

    updated file

--- express-demo-module3/index.js
+++ express-demo-module3/index.js
@@ -1,5 +1,8 @@
 const request=require('request');
-const url='http://localhost:8080/getMovies';
+const url='https://pro.openweathermap.org/data/2.5/climate/month?q=London&appid=01fc54654d0fa64b2085b411513c0d42';
+/*
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+*/
 
 request(url,{json:true},(err,res,body)=>{
 
