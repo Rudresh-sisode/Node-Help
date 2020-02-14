e563590c73dce3b4932d345654032f4c2ca085df
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-14 08:15:09 +0530

    database added

---
+++ mongo-node-module-4/db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/evs')
+
+//timestamp is 1:20:11
--- mongo-node-module-4/mongo-client.js
+++ mongo-node-module-4/mongo-client.js
@@ -11,6 +11,26 @@ MongoClient.connect(url,(err,db)=>{
         console.log('error while connecting to DB')
     }
     else{
-        console.log()
+        console.log('db connected successfully');
+        var collection=db.collection('loginuser')
+        //console.log(collection)
+
+        collection.find((error,data)=>{
+            if(error){
+                console.log("error while connecting")
+            }
+            else{
+                console.log("Data receiced from loginusers collection");
+              collection.insert({"name":"Rudresh","password":"rudresh123","admin":true},(er,result)=>{
+
+                if(er){
+                    console.log('insert fails')
+                }
+                else{
+                    console.log("successfully inserted")
+                }
+              })
+            }
+        })
     }
 })
