b314741984af052247a3a9e23069be91cfb085ff
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-16 17:57:16 +0530

    server file for router

--- mongo-node-module-4/server.js
+++ mongo-node-module-4/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser=require('body-parser');
-
-
 var db=require('./db')
 var user=require('./module/user');
 const app=express();
 const port=process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/login.html')
+})
